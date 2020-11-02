7981f5880a494f7ab2459355c666468ca52db989
Luciano Pierdona <lucianompj1@gmail.com>
2020-11-01 21:55:43 -0300

    added product component, rating component, bootstrap and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+
 const App = () => {
   return (
-    <>
-      <h1>Welcome to proshop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
