eb527984fbccc829abb22ebf8aea80b3fd21f53a
Luciano Pierdona <lucianompj1@gmail.com>
2020-11-04 23:16:14 -0300

    added middleware and user

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,8 +11,8 @@ const protect = expressAsync(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      
-      req.user = await (await User.findById(decoded.id)).select("-password");
+
+      req.user = await User.findById(decoded.id).select("-password");
 
       next();
     } catch (err) {
