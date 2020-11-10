55230d8edd2830cbe7f91755c82f155af5220172
Luciano Pierdona <lucianompj1@gmail.com>
2020-11-09 21:47:30 -0300

    added update order to paid controller

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 const router = express.Router();
 import { protect } from "../middleware/authMiddleware.js";
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderController.js";
 
 router.route("/").post(protect, addOrderItems);
 
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").post(protect, updateOrderToPaid);
 
 export default router;
