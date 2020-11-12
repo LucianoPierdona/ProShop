412b3cb12a942e1ef7f665d735dcd787652e363a
Luciano Pierdona <lucianompj1@gmail.com>
2020-11-11 21:50:27 -0300

    user edit screen and get details redux

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -66,7 +66,7 @@ const UserListScreen = ({ history }) => {
                   )}
                 </td>
                 <td>
-                  <LinkContainer to={`/user/${user._id}/edit`}>
+                  <LinkContainer to={`admin/user/${user._id}/edit`}>
                     <Button variant="light" className="btn-sm">
                       <i className="fas fa-edit"></i>
                     </Button>
@@ -76,7 +76,7 @@ const UserListScreen = ({ history }) => {
                     className="btn-sm"
                     onClick={() => {
                       deleteHandler(user._id);
-                      console.log("aqui");
+                      
                     }}
                   >
                     <i className="fas fa-trash"></i>
