0687af1f971c2cb19e1a5618fe4301c7b9c40c29
LucianoPierdona <lucianompj1@gmail.com>
2020-11-17 22:42:49 -0300

    create and update product endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -39,4 +39,58 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const createdProduct = await product.save();
+    return res.status(201).json(createdProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// @desc Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpeg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+
+  return res.status(201).json(createdProduct);
+});
+
 export { getProducts, getProductById, deleteProduct };
