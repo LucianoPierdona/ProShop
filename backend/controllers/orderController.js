99c4b5d5f762da006dea97240c662c740c70e3db
LucianoPierdona <lucianompj1@gmail.com>
2020-11-21 22:21:29 -0300

    admin order list backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -90,4 +90,19 @@ const getMyOrders = asyncHandler(async (req, res) => {
   return res.status(200).json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+// @desc Get All orders
+// @route GET /api/orders/
+// @access Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find().populate("user", "id name");
+
+  return res.status(200).json(orders);
+});
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { protect } from "../middleware/authMiddleware.js";
+import { admin, protect } from "../middleware/authMiddleware.js";
 import {
   addOrderItems,
   getMyOrders,
   getOrderById,
+  getOrders,
   updateOrderToPaid,
 } from "../controllers/orderController.js";
 
-router.route("/").post(protect, addOrderItems);
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route("/myorders").get(protect, getMyOrders);
 
 router.route("/:id").get(protect, getOrderById);
