2f4b11b59a3a6d463cff70ed90c263bf8f548e9f
LucianoPierdona <lucianompj1@gmail.com>
2020-12-07 10:47:03 -0300

    changed imports and added carousel styling

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Carousel, Image } from "react-bootstrap";
 import { listTopProducts } from "../actions/productActions";
 import Loader from "./Loader";
 import Message from "./Message";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ProductCarousel = () => {
   const dispatch = useDispatch();
